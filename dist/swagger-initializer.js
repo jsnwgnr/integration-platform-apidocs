14fcc20c4df64fcd36cd584f2660f9f1869afe11
Jason Wagner <42873185+jsnwgnr@users.noreply.github.com>
2022-12-19 09:19:12 -0600

    multiple api definitions

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,10 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    urls: [
+		{url: "apis/accountManager.yaml", name: "Account Manager"},
+		{url: "apis/agentManager.yaml", name: "Agent Manager"}
+	],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
